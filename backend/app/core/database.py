b7bd685882a61ed748cd51c9a433d7369b2163d6
Not4Rohith <tanuja.ramangoud@gmail.com>
2026-02-27 21:27:45 +0530

    minor issue fix

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -18,3 +18,11 @@ else:
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# Dependency to get the database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
